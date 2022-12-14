e476cd889161e68e98186bedab24863da203321a
qiujiajin <609799548@qq.com>
2022-12-14 19:34:34 +0800

    Feat: SayHi 现在支持给指定人打招呼啦

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/qiujiajin/hello/v2
+
+go 1.19
--- hello.go
+++ hello.go
@@ -2,7 +2,7 @@ package hello
 
 import "fmt"
 
-func SayHi() {
-	fmt.Println("你好，我是 秋风，很高兴认识你。")
+func SayHi(name string) {
+	fmt.Printf("你好(%s)，我是 秋风，很高兴认识你。\n", name)
 }
 
